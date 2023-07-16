101fac74064a396530793bf407e3ff0a56ec998c
John Firebaugh <69170718+jsf1515@users.noreply.github.com>
2023-07-16 12:46:53 -0400

    Update day1.js

--- 2021/day1.js
+++ 2021/day1.js
@@ -16,6 +16,18 @@ function part1() {
     return increaseCounter
 }
 
+function part1_2() {
+    const fs = require("fs")
+    return fs
+        .readFileSync("inputs.txt", "utf8")
+        .split("\n")
+        .map(Number)
+        .filter((value, index, origArray) => {
+            return index > 0 && value > origArray[index - 1]
+        })
+        .length
+}
+
 function part2() {
     const fs = require("fs")
     const data = fs
@@ -37,5 +49,22 @@ function part2() {
     return increaseCounter
 }
 
+function part2_2() {
+    const fs = require("fs")
+    const data = fs
+        .readFileSync("inputs.txt", "utf8")
+        .split("\n")
+        .map(Number)
+        .filter((value, index, origArray) => {
+            return index > 2 && (origArray[index] + origArray[index-1] + origArray[index-2]) > (origArray[index-1] + origArray[index-2] + origArray[index-3])
+        })
+        .length
+
+    return data
+}
+
 console.log(part1())
+console.log(part1_2())
+console.log("------------------")
 console.log(part2())
+console.log(part2_2())
